f2c908359bc7887c5e31b33f36c9fe900794d859
Dr. Volker Göbbels <vmg@arachnion.de>
2019-03-29 22:04:51 +0100

    Update

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+os.system("docker-compose build")
+os.system("docker-compose run --rm web django-admin.py startproject {{ cookiecutter.project_name }} .")
+
+#file = open("{{ cookiecutter.project_slug }}/settings.py", "r")
+
+#from_text = """        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),"""
+
+#to_text = """        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'HOST': 'db',
+#        'PORT': 5432,"""
+
+#content = file.read().replace(from_text, to_text)
+
+#file.close()
--- hooks/post_gen_project.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "PROJECT {{ cookiecutter.project_slug }} DONE"
-exit 0
