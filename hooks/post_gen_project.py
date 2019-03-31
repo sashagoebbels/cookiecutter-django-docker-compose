ccb5544e2161ded4dd13f8cb5f8252af638b9a87
Dr. Volker Göbbels <vmg@arachnion.de>
2019-03-31 17:04:06 +0200

    Update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -26,5 +26,5 @@ file.write(content)
 file.close()
 
 os.system("docker-compose run --rm web python manage.py startapp {{ cookiecutter.app_name }}")
-os.system("docker-compose up -d && sleep 10 && docker-compose run --rm web python manage.py migrate && docker-compose run --rm web python manage.py createsuperuser && docker-compose down")
+os.system("docker-compose up -d && sleep 15 && docker-compose run --rm web python manage.py migrate && docker-compose run --rm web python manage.py createsuperuser && docker-compose down")
 #os.system("./post_stuff.sh")
