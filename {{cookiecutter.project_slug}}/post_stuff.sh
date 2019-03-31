0e1ec90c647b04c0127b93ea69a419889a2cdb65
Dr. Volker Göbbels <vmg@arachnion.de>
2019-03-31 17:02:30 +0200

    Update

--- {{cookiecutter.project_slug}}/post_stuff.sh
+++ {{cookiecutter.project_slug}}/post_stuff.sh
@@ -1,7 +1,5 @@
 docker-compose up -d
 sleep 10 # PostgreSQL in a docker container tends to be a bit slow in startup
-#cid=`docker container ls | grep {{ cookiecutter.project_name }}_web | awk '{ print $1 }'`
-#docker exec $cid python manage.py migrate
 docker-compose run --rm web python manage.py migrate
 docker-compose run --rm web python manage.py createsuperuser
 docker-compose down
