468b0329818f85d774f83691fc23e1ecbea072df
Dr. Volker Göbbels <vmg@arachnion.de>
2019-04-06 16:37:31 +0200

    Update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,8 @@ import sys, os
 
 os.system("docker-compose build")
 os.system("docker-compose run --rm web django-admin.py startproject {{ cookiecutter.project_name }} .")
+os.system("docker-compose run --rm web python manage.py startapp {{ cookiecutter.app_name }}")
+os.mkdir('{{ cookiecutter.app_name }}/static')
 
 file = open("{{ cookiecutter.project_slug }}/settings.py", "r")
 from_text = """        'ENGINE': 'django.db.backends.sqlite3',
@@ -16,6 +18,7 @@ to_text = """        'ENGINE': 'django.db.backends.postgresql',
 content = file.read().replace(from_text, to_text)
 from_text = "STATIC_URL = '/static/'"
 to_text = """STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '{{ cookiecutter.app_name }}/static')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'"""
@@ -25,14 +28,4 @@ file = open("{{ cookiecutter.project_slug }}/settings.py", "w")
 file.write(content)
 file.close()
 
-os.system("docker-compose run --rm web python manage.py startapp {{ cookiecutter.app_name }}")
 os.system("docker-compose up -d && sleep 25 && docker-compose run --rm web python manage.py migrate && docker-compose run --rm web python manage.py createsuperuser && docker-compose down")
-
-os.mkdir('{{ cookiecutter.app_name }}/static')
-file = open("{{ cookiecutter.project_slug }}/settings.py", "r")
-from_text = "STATIC_URL = '/static/'"
-to_text = """STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '{{ cookiecutter.app_name }}/static')
-"""
-content = content.replace(from_text, to_text)
-file.close()
